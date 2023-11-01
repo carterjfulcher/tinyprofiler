273c2cab2b05cf9092efe83dfb50da96a288f02c
Carter Fulcher <carter@fulchereng.com>
2023-11-01 18:03:19 -0400

    release

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tinyprofiler",
+    version="0.1.0",
+    description="A Profiling Library for Python",
+    author="Carter Fulcher",
+    author_email="fulcher.carter@gmail.com",
+    packages=find_packages(),
+)
