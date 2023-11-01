0dc4001fdc1e619eacb7e7f65b2b6804643c70c8
Carter Fulcher <carter@fulchereng.com>
2023-11-01 18:18:24 -0400

    add readme to description

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="tinyprofiler",
-    version="0.1.4",
+    version="0.1.5",
     description="A Profiling Library for Python",
+    long_description=open("README.md").read(),
     author="Carter Fulcher",
     author_email="fulcher.carter@gmail.com",
     packages=find_packages(),
