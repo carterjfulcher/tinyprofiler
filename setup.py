beeff77834493e6132c1b1b411674af4156e1910
Carter Fulcher <carter@fulchereng.com>
2023-11-01 18:20:16 -0400

    fix readme bug

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1.5",
     description="A Profiling Library for Python",
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Carter Fulcher",
     author_email="fulcher.carter@gmail.com",
     packages=find_packages(),
