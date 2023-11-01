61c5f74ddf769c5642a6f132d396fc85659b67a8
Carter Fulcher <carter@fulchereng.com>
2023-11-01 18:43:50 -0400

    add totals

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tinyprofiler",
-    version="0.1.5",
+    version="0.1.6",
     description="A Profiling Library for Python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
